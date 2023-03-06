382e966d29b2a428136b9acedd8b3b077893ba51
Laszlo-Caballero <laszlocaballero@gmail.com>
2023-03-05 19:03:03 -0500

    upadated

--- app.js
+++ app.js
@@ -4,14 +4,23 @@ const sqlC = require("./config/sqlC");
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT;
+const fs = require("fs");
+const https = require("https");
 
 console.clear();
-
 sqlC();
-
+app.use(cors());
 app.use("/api", require("./routers"));
 app.use(express.static("storage"));
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+https
+  .createServer(
+    {
+      key: fs.readFileSync("server.key"),
+      cert: fs.readFileSync("server.cer"),
+    },
+    app
+  )
+  .listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+  });
